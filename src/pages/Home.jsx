ca4e135ff260c42ba6b58dffa906326cda9d8ebc
OliBoucher04 <oli_boucher@outlook.com>
2024-03-18 21:44:14 -0400

    add

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,83 @@
-import React from "react";
+// import React, { useEffect, useRef } from "react";
+// import { windowsXp } from "../assets";
+// import { FaFolder } from "react-icons/fa";
+// import "./Home.css";
+
+// const Layout = () => {
+//   const folderRef = useRef();
+//   const containerRef = useRef();
+
+//   useEffect(() => {
+//     if (!folderRef.current || !containerRef.current) return;
+
+//     const folder = folderRef.current;
+//     const container = containerRef.current;
+    
+//     folder.addEventListener('mousedown', () => {
+//       console.log('clicked');
+//     });
+
+//     return () => {
+//       folder.removeEventListener('mousedown', () => {
+//         console.log('clicked');
+//       });
+//     };
+//   }, []);
+
+//   return (
+//     <section className="w-screen h-screen">
+//       <img
+//         src={windowsXp}
+//         alt="fondEcran"
+//         className="fixed -z-[50] h-screen object-cover w-screen"
+//       />
+//       <div ref={containerRef} className="container relative overflow-hidden border-2 border-black w-screen h-screen">
+//         <FaFolder ref={folderRef} className="text-amber-300 text-6xl folder absolute top-0 left-0 cursor-pointer" />
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Layout;
+
+import React, {useState} from 'react';
+import Draggable from 'react-draggable';
+import { FaFolder } from "react-icons/fa";
 import { windowsXp } from "../assets";
 
-const Layout = () => {
-  return (
-    <>
-        <img src={windowsXp} alt="fondEcran" className="fixed"/>
+const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-    </>
+  const openPopup = () => {
+    setIsOpen(!isOpen); // Inverse la valeur actuelle de isOpen
+  }
+
+  return (
+    <section className="w-screen h-screen relative">
+      <img
+        src={windowsXp}
+        alt="fondEcran"
+        className="fixed -z-[50] h-screen object-cover w-screen"
+      />
+      <Draggable
+        axis="both"
+        handle=".handle"
+        defaultPosition={{x: 0, y: 0}}
+        position={null}
+        grid={[25, 25]}
+        scale={1}>
+        <div>
+          <div className="handle" onDoubleClick={openPopup}>
+            <FaFolder className="text-amber-300 text-6xl folder absolute top-0 left-0 cursor-pointer" />
+          </div>
+        </div>
+      </Draggable>
+      {isOpen &&
+        <div className='bg-white text-black absolute top-0 left-0 mt-10 ml-10 p-4'>Popup Content</div>
+      }
+    </section>
   );
 };
 
-export default Layout;
+export default Home;
+
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import './Login.css'
+import "./Login.css";
 import { AiFillLock } from "react-icons/ai";
+import { AiFillUnlock } from "react-icons/ai";
+
 
 const Login = () => {
-    return (
-        <section className="flex flex-col justify-center items-center h-screen">
-            <div class="input-container">
-                <input type="text" name="text" class="input" placeholder="search..." />
-                <span class="icon">
-                    <AiFillLock />
-                </span>
+  const [password, setPassword] = useState("");
+
+  const isPasswordInvalid = password.trim() === "";
+
+  return (
+    <section className="flex flex-col justify-center items-center h-screen">
+      <div class="input-container mb-8">
+        <input
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          type="password"
+          name="text"
+          className="input"
+          placeholder="***************"
+        />
+      </div>
+
+      {isPasswordInvalid ? (
+        <>
+          <div class="btn-container-disabled">
+            <a class="btn-content" href="#">
+              <AiFillLock className="h-[30px]"/>
+            </a>
+          </div>
+        </>
+      ) : (
+        <Link to="/home">
+          <div class="btn-container">
+            <div class="btn-content flex justify-center items-center relative transition-all" href="#">
+              <AiFillLock className="h-[30px] hover:hidden"/>
+              <AiFillUnlock className="h-[30px]"/>
             </div>
-            <Link to='/home'>Aller à l'accueil</Link>
-        </section>
-    );
+          </div>
+        </Link>
+      )}
+    </section>
+  );
 };
 
 export default Login;
