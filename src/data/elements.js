e14ceb40959328c680d7d78f5c3cb90038a049a8
OliBoucher04 <oli_boucher@outlook.com>
2024-03-19 11:22:06 -0400

    feature-structure-base-js

---
+++ src/data/elements.js
@@ -0,0 +1,45 @@
+[
+    {
+      "id": 1,
+      "video": "video1",
+      "code": "123456",
+      "photo": "photo1"
+    },
+    {
+      "id": 2,
+      "video": "video2",
+      "code": "223456",
+      "photo": "photo2"
+    },
+    {
+      "id": 3,
+      "video": "video3",
+      "code": "323456",
+      "photo": "photo3"
+    },
+    {
+      "id": 4,
+      "video": "video4",
+      "code": "423456",
+      "photo": "photo4"
+    },
+    {
+      "id": 5,
+      "video": "video5",
+      "code": "523456",
+      "photo": "photo5"
+    },
+    {
+      "id": 6,
+      "video": "video6",
+      "code": "623466",
+      "photo": "photo6"
+    },
+    {
+      "id": 7,
+      "video": "video7",
+      "code": "723476",
+      "photo": "photo7"
+    },
+  ]
+  
